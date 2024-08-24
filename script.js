ecdec512ecfffbd5440e198efc95955d90541c6f
vieradot <felipevieira013@hotmail.com>
2024-08-24 13:15:20 -0300

    all

---
+++ script.js
@@ -0,0 +1,29 @@
+var textRGB = document.getElementsByClassName('rgb-colorscheme');
+var i;
+
+function showRGB(){
+    for(i=0;i < textRGB.length ;i++){
+        textRGB[i].classList.add('rgb-show');
+    }
+}
+function hideRGB(){
+    for(i=0; i < textRGB.length; i++){
+        textRGB[i].classList.remove('rgb-show');
+    }
+};
+
+
+
+
+/* DARK MODE  START */
+var bodyDark = document.body;
+var welcomeBoxDark = document.getElementById('welcome-screen');
+var welcomeTextDark = document.getElementById('title');
+var headerDark = document.getElementById('header');
+
+function toggleDarkMode(){
+    bodyDark.classList.toggle('body-dark');
+    welcomeBoxDark.classList.toggle('welcome-screen-dark');
+    welcomeTextDark.classList.toggle('welcome-text-dark');
+    headerDark.classList.toggle('header-dark');
+};
